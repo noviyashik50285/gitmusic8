8f359062b6d0044ce11ad13f9a6603f64120c631
noviyashik50285 <mynewdisk50285@gmail.com>
2024-11-27 16:52:10 +0300

    first commit

--- backgr.js
+++ backgr.js
@@ -2,111 +2,179 @@ const button0 = document.getElementById('butn0');
 button0.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button1 = document.getElementById('butn1');
 button1.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong0.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button2 = document.getElementById('butn2');
 button2.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong1.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button3 = document.getElementById('butn3');
 button3.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong2.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button4 = document.getElementById('butn4');
 button4.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong3.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button5 = document.getElementById('butn5');
 button5.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong4.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button6 = document.getElementById('butn6');
 button6.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong5.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button7 = document.getElementById('butn7');
 button7.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong6.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button8 = document.getElementById('butn8');
 button8.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong7.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button9 = document.getElementById('butn9');
 button9.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong8.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button10 = document.getElementById('butn10');
 button10.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong9.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
-
 const button11 = document.getElementById('butn11');
 button11.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong10.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button12 = document.getElementById('butn12');
 button12.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong11.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button13 = document.getElementById('butn13');
 button13.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong12.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button14 = document.getElementById('butn14');
 button14.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong13.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button15 = document.getElementById('butn15');
 button15.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong14.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button16 = document.getElementById('butn16');
 button16.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong15.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button17 = document.getElementById('butn17');
 button17.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong16.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button18 = document.getElementById('butn18');
 button18.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong17.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button19 = document.getElementById('butn19');
 button19.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong18.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button20 = document.getElementById('butn20');
 button20.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong19.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button21 = document.getElementById('butn21');
 button21.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/halong20.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
+
+var obnulenie2 = function(){
+  button0.style.color = 'red';
+  button1.style.color = 'red';
+  button2.style.color = 'red';
+  button3.style.color = 'red';
+  button4.style.color = 'red';
+  button5.style.color = 'red';
+  button6.style.color = 'red';
+  button7.style.color = 'red';
+  button8.style.color = 'red';
+  button9.style.color = 'red';
+  button10.style.color = 'red';
+  button11.style.color = 'red';
+  button12.style.color = 'red';
+  button13.style.color = 'red';
+  button14.style.color = 'red';
+  button15.style.color = 'red';
+  button16.style.color = 'red';
+  button17.style.color = 'red';
+  button18.style.color = 'red';
+  button19.style.color = 'red';
+  button20.style.color = 'red';
+  button21.style.color = 'red';
+}
 
