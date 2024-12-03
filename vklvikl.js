74c2b2c4e6f06a56bd59a5706f307e7e049407b4
noviyashik50285 <mynewdisk50285@gmail.com>
2024-12-03 23:21:43 +0300

    sec6mus8 commit

--- vklvikl.js
+++ vklvikl.js
@@ -1,3 +1,4 @@
+//document.getElementById("knopa").style.display="none";
 var a;
 function show_hide()
 {
@@ -8,6 +9,57 @@ if(a==1) {
 else{
     document.getElementById("containerop").style.display="none";
     return a=1;
+}
+}
+
+var b;
+function show_hide2()
+{
+if(b==1) {
+    document.getElementById("container").style.display="inline";
+    return b=0;
+}
+else{
+    document.getElementById("container").style.display="none";
+    return b=1;
+}
+}
+
+var k;
+function show_hide3()
+{
+if(k==1) {
+    document.getElementById("butnfon").style.display="inline";
+    document.getElementById("butnfon2").style.display="inline";
+    return k=0;
+}
+else{
+    document.getElementById("butnfon").style.display="none";
+    document.getElementById("butnfon2").style.display="none";
+    return k=1;
+}
+}
+
+const makefon = document.getElementById("makefon");
+const containerop = document.getElementById("containerop");
+makefon.addEventListener('click', function() {
+    if (containerop.style.backgroundColor == 'green') {
+        containerop.style.backgroundColor = null;
+    } else {
+        containerop.style.backgroundColor = 'green';
+    }
+});
+
+var vse;
+function show_hide5()
+{
+if(vse==1) {
+    document.getElementById("victor1").style.display="inline";
+    return vse=0;
+}
+else{
+    document.getElementById("victor1").style.display="none";
+    return vse=1;
 }
 }
 
