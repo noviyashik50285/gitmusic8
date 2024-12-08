525228663168fc771a51472cf3aa26607f6aa313
noviyashik50285 <mynewdisk50285@gmail.com>
2024-12-09 00:44:35 +0300

    sec7 commit

---
+++ backgrk.js
@@ -0,0 +1,204 @@
+const button1 = document.getElementById('butn01');
+button1.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim01").style.display = "block";
+  this.style.color = 'blue';
+});
+const button2 = document.getElementById('butn02');
+button2.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim02").style.display = "block";
+  this.style.color = 'blue';
+});
+const button3 = document.getElementById('butn03');
+button3.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim03").style.display = "block";
+  this.style.color = 'blue';
+});
+const button4 = document.getElementById('butn04');
+button4.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim04").style.display = "block";
+  this.style.color = 'blue';
+});
+const button5 = document.getElementById('butn05');
+button5.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim05").style.display = "block";
+  this.style.color = 'blue';
+});
+const button6 = document.getElementById('butn06');
+button6.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim06").style.display = "block";
+  this.style.color = 'blue';
+});
+const button7 = document.getElementById('butn07');
+button7.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim07").style.display = "block";
+  this.style.color = 'blue';
+});
+const button8 = document.getElementById('butn08');
+button8.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim08").style.display = "block";
+  this.style.color = 'blue';
+});
+const button9 = document.getElementById('butn09');
+button9.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim09").style.display = "block";
+  this.style.color = 'blue';
+});
+const button10 = document.getElementById('butn10');
+button10.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim10").style.display = "block";
+  this.style.color = 'blue';
+});
+const button11 = document.getElementById('butn11');
+button11.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim11").style.display = "block";
+  this.style.color = 'blue';
+});
+const button12 = document.getElementById('butn12');
+button12.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim12").style.display = "block";
+  this.style.color = 'blue';
+});
+const button13 = document.getElementById('butn13');
+button13.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim13").style.display = "block";
+  this.style.color = 'blue';
+});
+const button14 = document.getElementById('butn14');
+button14.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim14").style.display = "block";
+  this.style.color = 'blue';
+});
+const button15 = document.getElementById('butn15');
+button15.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim15").style.display = "block";
+  this.style.color = 'blue';
+});
+const button16 = document.getElementById('butn16');
+button16.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim16").style.display = "block";
+  this.style.color = 'blue';
+});
+const button17 = document.getElementById('butn17');
+button17.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim17").style.display = "block";
+  this.style.color = 'blue';
+});
+const button18 = document.getElementById('butn18');
+button18.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim18").style.display = "block";
+  this.style.color = 'blue';
+});
+const button19 = document.getElementById('butn19');
+button19.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim19").style.display = "block";
+  this.style.color = 'blue';
+});
+const button20 = document.getElementById('butn20');
+button20.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim20").style.display = "block";
+  this.style.color = 'blue';
+});
+const button21 = document.getElementById('butn21');
+button21.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim21").style.display = "block";
+  this.style.color = 'blue';
+});
+const button22 = document.getElementById('butn22');
+button22.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim22").style.display = "block";
+  this.style.color = 'blue';
+});
+
+var obnuleniefon = function(){
+  document.getElementById("prim01").style.display = "none";  
+  document.getElementById("prim02").style.display = "none";  
+  document.getElementById("prim03").style.display = "none";  
+  document.getElementById("prim04").style.display = "none";  
+  document.getElementById("prim05").style.display = "none";  
+  document.getElementById("prim06").style.display = "none";  
+  document.getElementById("prim07").style.display = "none";  
+  document.getElementById("prim08").style.display = "none";  
+  document.getElementById("prim09").style.display = "none";  
+  document.getElementById("prim10").style.display = "none";  
+  document.getElementById("prim11").style.display = "none";  
+  document.getElementById("prim12").style.display = "none";  
+  document.getElementById("prim13").style.display = "none";  
+  document.getElementById("prim14").style.display = "none";  
+  document.getElementById("prim15").style.display = "none";  
+  document.getElementById("prim16").style.display = "none";  
+  document.getElementById("prim17").style.display = "none";  
+  document.getElementById("prim18").style.display = "none";  
+  document.getElementById("prim19").style.display = "none";  
+  document.getElementById("prim20").style.display = "none";  
+  document.getElementById("prim21").style.display = "none";  
+  document.getElementById("prim22").style.display = "none";  
+}
+var obnulenie2 = function(){
+  button1.style.color = 'red';
+  button2.style.color = 'red';
+  button3.style.color = 'red';
+  button4.style.color = 'red';
+  button5.style.color = 'red';
+  button6.style.color = 'red';
+  button7.style.color = 'red';
+  button8.style.color = 'red';
+  button9.style.color = 'red';
+  button10.style.color = 'red';
+  button11.style.color = 'red';
+  button12.style.color = 'red';
+  button13.style.color = 'red';
+  button14.style.color = 'red';
+  button15.style.color = 'red';
+  button16.style.color = 'red';
+  button17.style.color = 'red';
+  button18.style.color = 'red';
+  button19.style.color = 'red';
+  button20.style.color = 'red';
+  button21.style.color = 'red';
+  button22.style.color = 'red';
+}
+
